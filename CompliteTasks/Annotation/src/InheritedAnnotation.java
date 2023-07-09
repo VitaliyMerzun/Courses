fd525335f54414e3be13865f414da873a16a3ccd
VitaliyMerzun <139064265+VitaliyMerzun@users.noreply.github.com>
2023-07-09 21:04:34 +0300

    Delete CompliteTasks/Annotation directory

--- CompliteTasks/Annotation/src/InheritedAnnotation.java
+++
@@ -1,7 +0,0 @@
-import java.lang.annotation.*;
-
-@Inherited
-@Retention(RetentionPolicy.RUNTIME)
-@interface InheritedAnnotation {
-    String value();
-}
--- CompliteTasks/Annotation/src/RepeatDataContainers.java
+++
@@ -1,7 +0,0 @@
-
-import java.lang.annotation.*;
-import java.lang.reflect.*;
-@Retention(RetentionPolicy.RUNTIME)
-@interface RepeatedDataContainers {
-    RepeatedDataContainer[] value();
-}
