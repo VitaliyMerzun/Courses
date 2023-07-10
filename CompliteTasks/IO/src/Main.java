2dd0662540ff41746fd3408561a0eb3075bf8b6f
VitaliyMerzun <139064265+VitaliyMerzun@users.noreply.github.com>
2023-07-11 02:56:11 +0300

    Add files via upload

---
+++ CompliteTasks/IO/src/Main.java
@@ -0,0 +1,12 @@
+import javax.xml.crypto.Data;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        DataContainer dataFile = new DataContainer();
+        dataFile.myMethod1();
+        dataFile.myMethod2();
+    }
+}
