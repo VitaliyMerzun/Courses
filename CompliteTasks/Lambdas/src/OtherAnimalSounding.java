dc6361e3aca73535c4919f9577b5be0931e87033
VitaliyMerzun <139064265+VitaliyMerzun@users.noreply.github.com>
2023-07-11 23:12:09 +0300

    Add files via upload

---
+++ CompliteTasks/Lambdas/src/OtherAnimalSounding.java
@@ -0,0 +1,10 @@
+public class OtherAnimalSounding {
+
+    public static void soundDolphin() {
+        System.out.println("Click");
+    }
+
+    public static void soundOwl() {
+        System.out.println("Hood");
+    }
+}
